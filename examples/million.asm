08a586c2cb5f8c95da2fd4b24fa9daca1ac1c2f3
Anton Belyy <anton.belyy@gmail.com>
2013-01-21 18:34:42 +0400

    speed improvement and moving to libc functions

--- examples/million.asm
+++ examples/million.asm
@@ -1,27 +1,23 @@
 include "libc.obj"
 
 ^itoa_buf       dq      0
-newline         db      0xA
 
 million = 1000000
 
 _start:         mov     rsi,1
                 mov     rdi,10
+                mov     rcx,million
+                mov     bl,'\n'
 .loop:          push    rdi
                 push    ^itoa_buf
                 push    rsi
                 call    itoa
                 add     rsp,24
-                mov     rcx,rax
-                mov     rdx,rbx
-                mov     rax,4
-                mov     rbx,1
-                int     0x32
-                mov     rax,4
-                mov     rcx,newline
-                mov     rdx,rbx
-                int     0x32
+                push    rax
+                call    printf
+                add     rsp,8
+                push    bl
+                call    putc
                 inc     rsi
-                cmp     rsi,million
-                jle     .loop
+                loop    .loop
                 ret
