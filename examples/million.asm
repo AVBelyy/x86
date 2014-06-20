752c7a1db3ed6f8391ef1244da31296bc97a004a
Anton Belyy <anton.belyy@gmail.com>
2014-06-20 04:04:55 +0400

    small optimization in examples/million.asm

--- examples/million.asm
+++ examples/million.asm
@@ -15,11 +15,9 @@ _start:         mov     rsi,1
                 add     rsp,24
                 pop     rcx
                 push    rax
+                mov     [rax+rbx], '\n'
                 call    printf
                 add     rsp,8
-                push    byte '\n'
-                call    putc
-                inc     rsp
                 inc     rsi
                 loop    .loop
                 ret
